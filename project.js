60cc9100656930d2cca8462fdd1c22c2212c87f4
YugRupareliya <yugrupareliya100@gmail.com>
2025-03-29 16:25:18 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,7 @@
+//making create colour picker using dom
+document.getElementById("clrpicker").addEventListener("input",()=>{
+    let clr = document.getElementById("clrpicker").value;
+    document.body.style.backgroundColor=clr;
+    document.getElementById("box").value= clr;
+    
+})
